3a678031a208be35f2bb4c450704f8cc5f845327
kalum_ost <kalum.ost@ucalgary.ca>
2024-10-15 17:13:20 -0400

    [Minor] Clean-up + swap to custom logger

--- run_ml_analysis.py
+++ run_ml_analysis.py
@@ -1,13 +1,15 @@
 import json
 import logging
 from argparse import ArgumentParser
-from email.policy import default
 from pathlib import Path
-from typing import Tuple, List, Callable, Any, Dict, NoReturn
+from typing import List, Callable, Any, Dict
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def parse_config_entry(config_key: str, json_dict: dict, config_dict: dict, checks: List[Callable[[Any, Dict], Any]]):
@@ -33,10 +35,10 @@ def parse_config_entry(config_key: str, json_dict: dict, config_dict: dict, chec
 def parse_config(config_path: Path) -> dict:
     # Check to confirm the file exists and is a valid file
     if not config_path.exists():
-        logging.error("JSON configuration file designated was not found; terminating")
+        LOGGER.error("JSON configuration file designated was not found; terminating")
         raise FileNotFoundError()
     if not config_path.is_file():
-        logging.error("JSON configuration file specified was a directory, not a file; terminating")
+        LOGGER.error("JSON configuration file specified was a directory, not a file; terminating")
         raise TypeError()
 
     # Attempt to load the files contents w/ JSON
@@ -44,12 +46,12 @@ def parse_config(config_path: Path) -> dict:
         try:
             config_data = json.load(json_file)
         except Exception as e:
-            logging.error("Failed to load JSON file, see error below; terminating")
+            LOGGER.error("Failed to load JSON file, see error below; terminating")
             raise e
 
     # If the JSON is not coded as a dictionary, raise an error
     if type(config_data) is not dict:
-        logging.error(f"JSON should be formatted as a dictionary, was formatted as a {type(config_data)}; terminating")
+        LOGGER.error(f"JSON should be formatted as a dictionary, was formatted as a {type(config_data)}; terminating")
         raise TypeError
 
     # Parsed config dictionary
@@ -60,12 +62,12 @@ def parse_config(config_path: Path) -> dict:
     def default_seed(v, _):
         if v is None:
             default = 71554
-            logging.warning(f"No entry for {config_key} was found, defaulting to {default}")
+            LOGGER.warning(f"No entry for {config_key} was found, defaulting to {default}")
             return default
         return v
     def check_int(v, _):
         if type(v) is not int:
-            logging.error(f"'{config_key}' specified in the configuration file was not an integer; terminating")
+            LOGGER.error(f"'{config_key}' specified in the configuration file was not an integer; terminating")
             raise TypeError
         return v
     parsed_config = parse_config_entry(config_key, config_data, parsed_config, [default_seed, check_int])
@@ -75,12 +77,12 @@ def parse_config(config_path: Path) -> dict:
     def default_empty_list(v, _):
         if v is None:
             default = []
-            logging.warning(f"No entry for {config_key} was found, defaulting to empty list")
+            LOGGER.warning(f"No entry for {config_key} was found, defaulting to empty list")
             return default
         return v
     def check_list(v, _):
         if type(v) is not list:
-            logging.error(f"'{config_key}' specified in the configuration file was not a list; terminating")
+            LOGGER.error(f"'{config_key}' specified in the configuration file was not a list; terminating")
             raise TypeError
         return v
     parsed_config = parse_config_entry(config_key, config_data, parsed_config, [default_empty_list, check_list])
@@ -91,12 +93,12 @@ def parse_config(config_path: Path) -> dict:
     def default_nullity(v, _):
         if v is None:
             default = 0.75
-            logging.warning(f"No entry for {config_key} was found, defaulting to {default}")
+            LOGGER.warning(f"No entry for {config_key} was found, defaulting to {default}")
             return default
         return v
     def check_float(v, _):
         if type(v) is not float:
-            logging.error(f"'{config_key}' specified in the configuration file was not a float; terminating")
+            LOGGER.error(f"'{config_key}' specified in the configuration file was not a float; terminating")
             raise TypeError
         return v
     parsed_config = parse_config_entry(config_key, config_data, parsed_config, [default_nullity, check_float])
@@ -110,7 +112,7 @@ def parse_config(config_path: Path) -> dict:
     def default_repeats(v, _):
         if v is None:
             default = 10
-            logging.warning(
+            LOGGER.warning(
                 f"'{config_key}' was not specified in the configuration file, defaulting to {default}.")
             return default
         return v
@@ -124,7 +126,7 @@ def parse_config(config_path: Path) -> dict:
     config_key = "target_column"
     def value_required(v, _):
         if v is None:
-            logging.error(f"Config value '{config_key}' must be specified by the user. Terminating.")
+            LOGGER.error(f"Config value '{config_key}' must be specified by the user. Terminating.")
             raise ValueError()
         return v
     def as_str(v, _):
@@ -134,7 +136,7 @@ def parse_config(config_path: Path) -> dict:
     # Warn the user of any unused entries in the config file
     if len(config_data) > 0:
         for k in config_data.keys():
-            logging.warning(
+            LOGGER.warning(
                 f"Entry '{k}' in configuration file did not match a valid configuration option and was skipped"
             )
 
@@ -185,7 +187,7 @@ def main(in_path: Path, out_path: Path, config: Path, debug: bool):
 
     if debug:
         presplit_out = "presplit.tsv"
-        logging.debug(f"Saving pre-split dataset to {presplit_out}")
+        LOGGER.debug(f"Saving pre-split dataset to {presplit_out}")
         df.to_csv(presplit_out, sep='\t')
 
     # Generate the requested number of different train-test split workspaces
@@ -203,7 +205,7 @@ def main(in_path: Path, out_path: Path, config: Path, debug: bool):
         test_df = df.loc[test_idx, :]
 
         # If debugging, report the sizes
-        logging.debug(f"Test/Train ration (split {i}): {len(test_idx)}/{len(train_idx)}")
+        LOGGER.debug(f"Test/Train ration (split {i}): {len(test_idx)}/{len(train_idx)}")
 
 
 
@@ -233,10 +235,16 @@ if __name__ == "__main__":
 
     argvs = parser.parse_args().__dict__
 
-    # Enable debug logging if requested
-    debug = argvs.get('debug', False)
-    if debug:
-        logging.root.setLevel(logging.DEBUG)
+    # Generate a logger for this program
+    logging.basicConfig(
+        format="{asctime} {levelname}:  \t{message}",
+        style='{',
+        datefmt="%H:%M:%S"
+    )
+    if argvs.get('debug', False):
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        LOGGER.setLevel(logging.INFO)
 
     # Run the analysis
     main(**argvs)

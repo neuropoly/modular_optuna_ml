1dabf4cb26801a1a55d2f4c13d4717b3920a0789
kalum.ost <kalum.ost@ucalgary.ca>
2024-11-04 15:22:10 -0500

    Added the RandomForestClassifier manager, the second ensemble model

--- models/sklearn/__init__.py
+++ models/sklearn/__init__.py
@@ -1,11 +1,12 @@
 from models.sklearn.linear import LogisticRegressionManager
 from models.sklearn.neighbors import KNeighborsClassifierManager
 from models.sklearn.svm import SVCManager
-from models.sklearn.ensemble import AdaBoostClassifierManager
+from models.sklearn.ensemble import AdaBoostClassifierManager, RandomForestClassifierManager
 
 MANAGER_MAP = {
     'LogisticRegression': LogisticRegressionManager,
     'SVC': SVCManager,
     'KNNC':  KNeighborsClassifierManager,
-    'AdaBoostClassifier': AdaBoostClassifierManager
+    'AdaBoostClassifier': AdaBoostClassifierManager,
+    'RFC': RandomForestClassifierManager
 }
--- models/sklearn/ensemble.py
+++ models/sklearn/ensemble.py
@@ -1,12 +1,12 @@
 from optuna import Trial
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
 from models.sklearn.base import SciKitLearnModelManager
 
 
 class AdaBoostClassifierManager(SciKitLearnModelManager[AdaBoostClassifier]):
     """
-    Optuna model manager for the KNeighborsClassifier class in SciKit-Learn
+    Optuna model manager for the AdaBoostClassifier class in SciKit-Learn
     """
     def build_model(self, trial: Trial):
         # Get the number of neighbors which should be used
@@ -17,3 +17,20 @@ class AdaBoostClassifierManager(SciKitLearnModelManager[AdaBoostClassifier]):
 
         # Return the resulting model
         return AdaBoostClassifier(n_estimators=n_estimators, learning_rate=learning_rate, algorithm="SAMME")
+
+class RandomForestClassifierManager(SciKitLearnModelManager[RandomForestClassifier]):
+    """
+    Optuna model manager for the RandomForestClassifier class in SciKit-Learn
+    """
+    def build_model(self, trial: Trial):
+        # Get the criterion to use decision tree splits
+        criterion = self.trial_closures['criterion'](trial)
+
+        # Get the minimum number of samples that need to be in each split in a decision tree
+        min_samples_split = self.trial_closures['min_samples_split'](trial)
+
+        # Get the maximum number of features that can be used by a tree at each step
+        max_features = self.trial_closures['max_features'](trial)
+
+        # Return the resulting model
+        return RandomForestClassifier(criterion=criterion, min_samples_split=min_samples_split, max_features=max_features)

0a451ddb806ffd97b76acfc4f28445bfa1c54c24
kalum.ost <kalum.ost@polymtl.ca>
2025-01-28 17:02:10 -0500

    New hook; literally just dumps the dataframe at that point to the file. VERY MUCH WIP (i.e. it will overwrite the output file repeatedly if the hook is called at any point past the first data split!)

---
+++ data/hooks/misc.py
@@ -0,0 +1,36 @@
+from logging import Logger
+from typing import Optional, Self
+
+from config.utils import default_as, parse_data_config_entry
+from data import BaseDataManager
+from data.hooks import registered_data_hook
+from data.hooks.base import DataHook
+from data.mixins import MultiFeatureMixin
+
+
+@registered_data_hook("dump")
+class DumpHook(DataHook):
+    def __init__(self, config: dict, **kwargs):
+        super().__init__(config, **kwargs)
+
+        # Grab an explicit list of columns, if they were defined
+        self.output_dest = parse_data_config_entry(
+            "output_dest", config,
+            default_as('./dumped.tsv', self.logger)
+        )
+
+    @classmethod
+    def from_config(cls, config: dict, logger: Logger = Logger.root) -> Self:
+        return cls(config, logger=logger)
+
+    def run(self,
+            x: BaseDataManager,
+            y: Optional[BaseDataManager] = None
+        ) -> BaseDataManager:
+        # We can only dump tabular datasets currently
+        if isinstance(x, MultiFeatureMixin):
+            df_out = x.data
+            df_out['target'] = y.as_array()
+            df_out.to_csv(self.output_dest, sep='\t')
+        # Return the result, unchanged
+        return x

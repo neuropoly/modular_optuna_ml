dec2def8f569bde3b0781003017ed77247677f31
kalum.ost <kalum.ost@ucalgary.ca>
2025-05-13 10:16:26 -0400

    Removed reference to MultiFeatureMixin in "feature_selection" hooks

--- data/hooks/feature_selection.py
+++ data/hooks/feature_selection.py
@@ -13,7 +13,6 @@ from config.utils import default_as, is_float, is_list, parse_data_config_entry
 from data.base import BaseDataManager
 from data.hooks import registered_data_hook
 from data.hooks.base import DataHook, FittedDataHook
-from data.mixins import MultiFeatureMixin
 from tuning.utils import Tunable, TunableParam
 
 
@@ -49,11 +48,10 @@ class ExplicitDrop(ExplicitFeatures):
       "features": ["foo", "bar"]
     }
     """
-    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager | MultiFeatureMixin:
+    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
         # We can only drop features in a dataset if they have more than 1
-        if not isinstance(x, MultiFeatureMixin):
+        if x.n_features() == 1:
             raise TypeError(f"DataManager of type '{self.__class__}' only has one feature! Cannot modify feature set.")
-        x: BaseDataManager | MultiFeatureMixin
         return x.drop_features(self.features)
 
 @registered_data_hook("keep_features_explicit")
@@ -67,11 +65,10 @@ class ExplicitKeep(ExplicitFeatures):
       "features": ["baz", "bing"]
     }
     """
-    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager | MultiFeatureMixin:
+    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
         # We can only drop features in a dataset if they have more than 1
-        if not isinstance(x, MultiFeatureMixin):
+        if x.n_features() == 1:
             raise TypeError(f"DataManager of type '{self.__class__}' only has one feature! Cannot modify feature set.")
-        x: BaseDataManager | MultiFeatureMixin
         return x.get_features(self.features)
 
 
@@ -107,11 +104,8 @@ class SampleNullityDrop(NullityDrop):
       "threshold": 0.5
     }
     """
-    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager | MultiFeatureMixin:
+    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
         # Iterate through all samples to calculate their null content, dropping any above the configured threshold
-        if not isinstance(x, MultiFeatureMixin):
-            raise ValueError(f"Sample-based nullity dropping only makes sense in the context of multi-feature data!")
-        x: BaseDataManager | MultiFeatureMixin
         null_tolerance = int(x.n_features() * self.threshold)
         keep_idx = [i for i, k in enumerate(x) if np.sum(pd.isnull(k.as_array())) < null_tolerance]
         return x[keep_idx]
@@ -129,11 +123,10 @@ class FeatureNullityDrop(NullityDrop):
       "threshold": 0.5
     }
     """
-    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager | MultiFeatureMixin:
+    def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
         # Make sure this DataManager has more than one feature!
-        if not isinstance(x, MultiFeatureMixin):
+        if x.n_features() == 1:
             raise TypeError(f"DataManager of type '{self.__class__.__name__}' only has one feature which cannot be dropped!")
-        x: BaseDataManager | MultiFeatureMixin
         # Otherwise, calculate the null content for each feature dropping any which surpass the configured threshold
         n_samples = len(x)
         null_tolerance = int(n_samples * self.threshold)
@@ -192,21 +185,14 @@ class PrincipalComponentAnalysis(Tunable, FittedDataHook):
         return [self.prop_tuner]
 
     def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
-        if isinstance(x, MultiFeatureMixin):
-            # Denote the type of our inputs so type hinting doesn't suck
-            x: MultiFeatureMixin | BaseDataManager
+        # Calculate and regenerate the features in the training set
+        tmp_train = self.backing_pca.fit_transform(x.as_array())
+        feature_labels = [f'PC{i}' for i in range(tmp_train.shape[1])]
 
-            # Calculate and regenerate the features in the training set
-            tmp_train = self.backing_pca.fit_transform(x.as_array())
-            feature_labels = [f'PC{i}' for i in range(tmp_train.shape[1])]
+        # Drop all features and replace them with the new components
+        x_out = x.drop_features(x.features())
+        x_out = x_out.set_features(feature_labels, tmp_train)
 
-            # Drop all features and replace them with the new components
-            x_out = x.drop_features(x.features())
-            x_out: BaseDataManager | MultiFeatureMixin = x_out.set_features(feature_labels, tmp_train)
-
-        # Otherwise, report an error, as we can't dimensionally reduce a single dimension!
-        else:
-            raise ValueError("Dimensionality reduction cannot be run on a dataset with only 1 dimension!")
         return x_out
 
     def run_fitted(self,
@@ -215,27 +201,19 @@ class PrincipalComponentAnalysis(Tunable, FittedDataHook):
                y_train: Optional[BaseDataManager] = None,
                y_test: Optional[BaseDataManager] = None
            ) -> (BaseDataManager, BaseDataManager):
-        if isinstance(x_train, MultiFeatureMixin):
-            # Denote the type of our inputs so type hinting doesn't suck
-            x_train: MultiFeatureMixin | BaseDataManager
-            x_test: MultiFeatureMixin | BaseDataManager
+        # Run the fitted analysis first
+        tmp_train = self.backing_pca.fit_transform(x_train.as_array())
+        feature_labels = [f'PC{i}' for i in range(tmp_train.shape[1])]
 
-            # Run the fitted analysis first
-            tmp_train = self.backing_pca.fit_transform(x_train.as_array())
-            feature_labels = [f'PC{i}' for i in range(tmp_train.shape[1])]
+        # Drop all features and replace them with the new components
+        train_out = x_train.drop_features(x_train.features())
+        train_out = train_out.set_features(feature_labels, tmp_train)
 
-            # Drop all features and replace them with the new components
-            train_out = x_train.drop_features(x_train.features())
-            train_out = train_out.set_features(feature_labels, tmp_train)
+        # Do the same to our testing data, but without re-fitting
+        tmp_test = self.backing_pca.transform(x_test.as_array())
+        test_out = x_test.drop_features(x_test.features())
+        test_out = test_out.set_features(feature_labels, tmp_test)
 
-            # Do the same to our testing data, but without re-fitting
-            tmp_test = self.backing_pca.transform(x_test.as_array())
-            test_out = x_test.drop_features(x_test.features())
-            test_out = test_out.set_features(feature_labels, tmp_test)
-
-        # Otherwise, report an error, as we can't dimensionally reduce a single dimension!
-        else:
-            raise ValueError("Dimensionality reduction cannot be run on a dataset with only 1 dimension!")
         return train_out, test_out
 
 
@@ -293,32 +271,25 @@ class RecursiveFeatureElimination(Tunable, FittedDataHook):
         return [self.prop_tuner]
 
     def run(self, x: BaseDataManager, y: Optional[BaseDataManager] = None) -> BaseDataManager:
-        if isinstance(x, MultiFeatureMixin):
-            # Denote the type of our inputs so type hinting doesn't suck
-            x: MultiFeatureMixin | BaseDataManager
+        # If x contains only one feature already, just return that feature, as RFE has a stroke otherwise
+        if x.n_features() == 1:
+            self.logger.warning("Only one feature in the dataset was found, making RFE redundant. "
+                                "Original (unmodified) dataset returned instead.")
+            self.selected_features = x.features()
+            return x
 
-            # If x contains only one feature already, just return that feature, as RFE has a stroke otherwise
-            if x.n_features() == 1:
-                self.logger.warning("Only one feature in the dataset was found, making RFE redundant. "
-                                    "Original (unmodified) dataset returned instead.")
-                self.selected_features = x.features()
-                return x
+        # If the number of features selected would be 0, make it 1 instead
+        if self.prop_tuner.value * x.n_features() < 1:
+            self.backing_rfe.n_features_to_select = 1
 
-            # If the number of features selected would be 0, make it 1 instead
-            if self.prop_tuner.value * x.n_features() < 1:
-                self.backing_rfe.n_features_to_select = 1
+        # Calculate and regenerate the features in the training set
+        self.backing_rfe.fit(x.as_array(), np.ravel(y.as_array())) # Ravel prevents some warning spam
+        self.selected_features = self.backing_rfe.get_feature_names_out(x.features())
 
-            # Calculate and regenerate the features in the training set
-            self.backing_rfe.fit(x.as_array(), np.ravel(y.as_array())) # Ravel prevents some warning spam
-            self.selected_features = self.backing_rfe.get_feature_names_out(x.features())
-
-            # Select only the features
-            x_out: BaseDataManager | MultiFeatureMixin = x.get_features(self.selected_features)
+        # Isolate these selected features from the rest of the data
+        x_out = x.get_features(self.selected_features)
 
         # Otherwise, raise an error, as it makes no sense to feature select a single feature!
-        else:
-            raise ValueError(f"The RFE data hook currently only supports multi-feature datasets; "
-                             f"received {type(x)} instead")
         return x_out
 
     def run_fitted(self,
@@ -327,19 +298,10 @@ class RecursiveFeatureElimination(Tunable, FittedDataHook):
                y_train: Optional[BaseDataManager] = None,
                y_test: Optional[BaseDataManager] = None
            ) -> (BaseDataManager, BaseDataManager):
-        if isinstance(x_train, MultiFeatureMixin):
-            # Denote the type of our inputs so type hinting doesn't suck
-            x_train: MultiFeatureMixin | BaseDataManager
-            x_test: MultiFeatureMixin | BaseDataManager
+        # Run the fitted analysis first
+        train_out = self.run(x_train, y_train)
 
-            # Run the fitted analysis first
-            train_out = self.run(x_train, y_train)
+        # Use the same set of features to filter the x_test set
+        test_out = x_test.get_features(self.selected_features)
 
-            # Use the same set of features to filter the x_test set
-            test_out = x_test.get_features(self.selected_features)
-
-
-        # Otherwise, report an error, as we can't dimensionally reduce a single dimension!
-        else:
-            raise ValueError("Dimensionality reduction cannot be run on a dataset with only 1 dimension!")
         return train_out, test_out
